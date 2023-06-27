707fe6e846dc2f173241dbe844b4d819903a9a99
Matt Miholics <mmiholics8@gmail.com>
2023-06-26 22:58:27 -0400

    yeah man

---
+++ my-app/src/App.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import './App.css';
+import {AiFillLinkedin} from 'react-icons/ai';
+import threeMonkeys from './threemonkeys.jpg';
+
+
+function App() {
+  return (
+  <> 
+  <Navbar />
+    <div className=" bg-mTan h-full w-full top-40 pt-20 pb-5 px-2">
+    <div id="TiTT"className="text-mPink bg-grimace rounded-lg text-2xl md:text-5xl py-10 px-4">
+          <h1 id="about" className="font-bold text-center text-5xl md:text-8xl">TROUBLE IN TORNADO TOWN</h1>
+          <h2 className="py-5  text-center font-bold">Release Trailer:</h2>
+          <iframe className='container mx-auto grow w-11/12 aspect-video self-center md:min-w-max' src="https://www.youtube.com/embed/yYihLYcPJls" title="Trouble in Tornado Town  - Release Trailer" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
+          <div className='py-5 block'><iframe className="h-48 overflow-hidden container mx-auto" src="https://store.steampowered.com/widget/2180060/" /></div>
+          <div className="">
+            <div className="py-2 justify-center">
+            <p className="py-5 px-5 ">Welcome to Tornado Town, a place so lovely you'll be blown away! Trouble in Tornado town is a physics-driven cartoon destruction game where you grow into the most powerful tornado ever seen!</p>
+            </div>
+            <div className="py-5 px-5">
+            <p>Play as a tornado and try to destroy as much of of the landscape as possible, as quickly as possible!</p>
+            </div>
+          </div>
+        </div>
+        <div className='py-3'></div>
+        <div id="about" className="text-mPink bg-grimace rounded-lg text-2xl md:text-5xl py-10 px-4 text-center items-center">
+          <h1 className="font-bold text-center text-6xl md:text-8xl">ABOUT</h1>
+          <img src ={threeMonkeys} className="py-4 inline-flex justify-center"/>
+          <p className="py-5 px-5 text-left">Abducted Monkeys is a small team of students developing our indie action game Trouble in Tornado Town. We strive to develop compact, enjoyable experiences for players of all skill levels.</p>
+          <p className="py-5 px-5 text-left">All of our developers are currently studying Game Design and Production at Drexel University. Trouble in Tornado Town was developed during the 3rd year of our studies and we owe a great deal of gratitude to our professors at Drexel's Entrepreneurial Game Studio for their advice and industry know-how.</p>
+          <div className="items-center my-3">
+            <div className="py-2">
+                <h2 className="py-2 text-center font-bold">Abducted Monkeys is:</h2>
+                <a href = "https://www.linkedin.com/in/spenceradamcohen/" className="py-5 justify-center md:px-4 flex text-mLink hover:text-mTan underline md:no-underline">Spencer Cohen</a>
+                <a href = "http://joshuapelican.github.io/" className="py-5 md:px-4 justify-center flex text-mLink hover:text-mTan underline md:no-underline">Joshua Pelkington</a>
+                <a href = "https://tomgia.com/" className="py-5 md:px-4 justify-center flex text-mLink hover:text-mTan underline md:no-underline">Tom Giagtzoglou</a>
+                <a href = "https://www.artstation.com/joyful2051" className="py-5 justify-center flex md:px-4 text-mLink hover:text-mTan underline md:no-underline">Joi Saito</a>
+                <a href = "https://www.artstation.com/danguad" className="py-5 justify-center flex md:px-4 text-mLink hover:text-mTan underline md:no-underline">Dan Guadalupe</a>
+                <a href = "https://www.linkedin.com/in/brian-glogower/" className="py-5 justify-center flex md:px-4 text-mLink hover:text-mTan underline md:no-underline">Brian Glogower</a>
+                <a href = "https://www.linkedin.com/in/jordan-gerhardt-91a5141a2/" className="py-5 justify-center flex md:px-4 text-mLink hover:text-mTan underline md:no-underline">Jordan Gerhardt</a>
+                <a href = "https://www.artstation.com/miholicsmatt" className="py-5 justify-center flex md:px-4 text-mLink hover:text-mTan underline md:no-underline">Matt Miholics</a>
+                <a href = "https://www.artstation.com/alexgjasmin" className="py-5 justify-center flex md:px-4 text-mLink hover:text-mTan underline md:no-underline">Alex Jasmin</a>
+            </div>
+
+            <div className="py-5">
+                <h2 className="py-2 text-center font-bold ">Mentored by:</h2>
+                <p className="py-5 justify-center flex md:px-4 text-mLink">Tony Rowe</p>
+                <p className="py-5 justify-center flex md:px-4 text-mLink">Frank Lee</p>
+            </div>
+          </div>
+        </div>
+        <div className='py-3'></div>
+        <div id="contact"className="text-mPink bg-grimace rounded-lg text-2xl md:text-5xl py-10 px-4 items-center text-center">
+          <h1 id="about" className="font-bold text-5xl md:text-8xl">CONTACT US</h1>
+          <h2 className="py-5 font-bold">You can contact us via Email or on Linkedin</h2>
+          <div className="items-center">
+            <p className="py-5 text-lg md:text-3xl">contact[at]abductedmonkeys.com</p>
+            <AiFillLinkedin className=' mx-auto text-5xl'/>
+          </div>
+        </div>
+    </div>
+    <Footer />
+  </>
+);
+}
+
+export default App;
