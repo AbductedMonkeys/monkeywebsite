f51828d0786bcc8481b07d5f5534ad7011c69749
Matt Miholics <mmiholics8@gmail.com>
2024-02-29 14:44:40 -0500

    swiper testing

--- src/components/CustSwiper.tsx
+++ src/components/CustSwiper.tsx
@@ -6,24 +6,27 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+import Image from "next/image";
+
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
-export default function CustSwiper() {
+const props = () =>{
+
+}
+
+export default function CustSwiper(pictures) {
   return (
     <Swiper 
     navigation={true}
     modules={[Pagination, Navigation]} 
     className="w-2xl bg-white">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+       {pictures.map((picture)=>{
+            <SwiperSlide key={picture.key}>
+              <Image src={picture.img} alt={picture.alt} />
+            </SwiperSlide>
+        }
+      )} 
     </Swiper>
   )
 }
