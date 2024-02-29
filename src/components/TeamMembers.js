2497e0b69b4288c17eaa54c418c7603ff8871588
Matt Miholics <mmiholics8@gmail.com>
2024-02-29 15:29:29 -0500

    more comments and teamcard file

---
+++ src/components/TeamMembers.js
@@ -0,0 +1,35 @@
+export const TeamPeople = [
+    { 
+    name: 'Spencer Cohen',
+    img: spenImg,
+    imgalt:"Spencer Image", //next images require this just do "{Person} Image"
+    title: "CEO/Producer",
+    links:[{
+        personal:"",
+        linkedin:"",
+        git:"",
+        artstation:"",
+    }],
+    games:[{
+        tornado: true,
+        lockjaw: true,
+    }]
+    },
+    { 
+        //second example for how to format curly braces
+        name: 'Spencer Cohen',
+        img: spenImg,
+        imgalt:"Spencer Image", //next images require this just do "{Person} Image"
+        title: "CEO/Producer",
+        links:[{
+            personal:"",
+            linkedin:"",
+            git:"",
+            artstation:"",
+        }],
+        games:[{
+            tornado: true,
+            lockjaw: true,
+        }]
+    },
+]
