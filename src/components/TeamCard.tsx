9020272caa8821fbba66a94781a73210042bfd31
Matt Miholics <mmiholics8@gmail.com>
2024-03-05 00:48:18 -0500

    card styles update

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -55,7 +55,7 @@ const renderTeamLinks = (links) => {
  const TeamCard = ({person}) => {
     return (
         <> 
-        <div className="bg-grimace lg:m-6 p-2 flex flex-wrap justify-left h-full rounded-md outline-4 outline md:outline-8 outline-mTan">
+        <div className="bg-grimace lg:m-6 p-2 flex flex-wrap justify-left h-full rounded-md items-center outline-4 outline md:outline-8 outline-mTan">
             <div className="w-6/12 h-fit sm:w-4/12 p-2 lg:p-4">
                 <Image className="shadow rounded-full max-w-full h-auto align-middle border-none" src={person.img} alt={person.imgalt}/>
             </div>
@@ -64,7 +64,9 @@ const renderTeamLinks = (links) => {
                     <h1 className="font-bold text-xl lg:text-3xl flex flex-shrink w-full">{person?.name}</h1>
                     <h2 className="font-bold text-sm lg:text-xl flex flex-shrink w-full">{person?.title}</h2>
                 </div>
-                <div className="inline-flex justify-between w-full h-fit">
+                
+            </div>
+            <div className="inline-flex justify-around w-full h-fit">
                     <div className="flex h-fit w-auto">
                         {renderTeamLinks(person.links[0])}
                     </div>
@@ -72,7 +74,6 @@ const renderTeamLinks = (links) => {
                         {renderGameIcons(person.games[0])}
                     </div>
                 </div>
-            </div>
         </div>
 
         
