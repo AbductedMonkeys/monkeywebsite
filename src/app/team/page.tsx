a37a3e54fc7ae3c0d37f67cf65ab1880da59c5ed
Matt Miholics <mmiholics8@gmail.com>
2024-03-05 01:26:26 -0500

    width and breakpoint update

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -12,10 +12,10 @@ export default function About() {
           <div className='mt-20 bg-grimace rounded-md w-1/2 h-fit'>
             <h1 className="py-5 font-bold text-center text-5xl lg:text-8xl">Team</h1>
           </div>
-          <div className='w-full md:w-1/2'>
+          <div className='w-full md:w-3/4'>
             <div className="items-center my-3">
               <div className="py-2">
-                <div className='grid grid-cols-1 2xl:grid-cols-2'>
+                <div className='grid grid-cols-1 xl:grid-cols-2'>
                   {TeamPeople.map((person)=>(
                       <div className='py-5' key={person.id}>
                       <TeamCard person={person}></TeamCard>
