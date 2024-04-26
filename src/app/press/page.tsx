0065d06fb969960776eaf25d6a8c037762f2ecf7
Matt Miholics <mmiholics8@gmail.com>
2024-04-25 23:58:25 -0400

    the press kits in question

--- src/app/press/page.tsx
+++ src/app/press/page.tsx
@@ -14,17 +14,17 @@ export default function Press() {
           <div className="w-full md:w-1/2 px-6 mb-6 md:mb-0 flex justify-center">
               <div className="w-full max-w-xs md:max-w-lg px-6 py-10 rounded-xl">
                 <p className="text-lg md:text-3xl text-center mb-6">Want to cover our games? Check out our press kits for media and info, or send us an email.</p>
-                <a href="https://drive.google.com/drive/folders/1XhlMzCkKwPJj6o7G-fsYHHS5hxFtRAZt?usp=sharing" target="_blank" rel="noopener noreferrer" className="block w-fit mx-auto outline outline-mPink outline-2 hover:bg-mPink hover:text-mTan font-bold text-center py-3 px-6 rounded-lg transition duration-300">Press Kit</a>
+                <a href="mailto:contact@abductedmonkeys.com" target="_blank" rel="noopener noreferrer" className="block w-fit mx-auto outline outline-mPink outline-2 hover:bg-mPink hover:text-mTan font-bold text-center py-3 px-6 rounded-lg transition duration-300">Press Kit</a>
               </div>
             </div>
           <div className="w-full lg:w-3/4 my-10 justify-center flex flex-wrap">
-            <a href="#" target="_blank" rel="noopener noreferrer" className="w-full md:w-1/2 lg:w-1/3 px-6  flex justify-center">
+            <a href="https://drive.google.com/drive/folders/1U8Fh48_GMZZqPVF2Gwe3rIJsbEdI00e8?usp=sharing" target="_blank" rel="noopener noreferrer" className="w-full md:w-1/2 lg:w-1/3 px-6  flex justify-center">
                 <div className="max-w-xs md:max-w-md px-6 py-10 rounded-xl text-center hover:text-mTan hover:bg-mPink hover:shadow-black hover:duration-200">
                   <Image alt="Abducted Monkeys Team" src={lockjawThum} className="rounded-2xl mx-auto mb-3" />
                   <p className="text-lg underline md:text-3xl">Lockjaw: Robo-Royale!</p>
                 </div>
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer" className="w-full md:w-1/2 lg:w-1/3 px-6  flex justify-center">
+            <a href="https://drive.google.com/drive/folders/1XhlMzCkKwPJj6o7G-fsYHHS5hxFtRAZt?usp=sharing" target="_blank" rel="noopener noreferrer" className="w-full md:w-1/2 lg:w-1/3 px-6  flex justify-center">
               <div className="max-w-xs md:max-w-md px-6 py-10 rounded-xl text-center hover:text-mTan hover:bg-mPink hover:shadow-black hover:duration-200">
                 <Image alt="Abducted Monkeys Team" src={tornadoThum} className="rounded-2xl mx-auto mb-3" />
                 <p className="text-lg underline  md:text-3xl">Trouble in Tornado Town</p>
